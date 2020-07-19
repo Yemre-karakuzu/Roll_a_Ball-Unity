350cfca2c1d5b3439cb2d7adb1f3371977ee334c
Yemre-karakuzu <yunus11414@gmail.com>
2020-07-20 00:00:41 +0300

    basit bir oyun

---
+++ Roll a Ball/Assets/Kodlar/TopKontrol.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TopKontrol : MonoBehaviour {
+
+    Rigidbody fizik;
+    public int hiz;
+    int sayac = 0;
+    public int toplanicakNesneSayisi;
+    public Text sayacText;
+    public Text OyunBittiText;
+    void Start ()
+    {
+        fizik = GetComponent<Rigidbody>();
+	}
+	
+	
+	void FixedUpdate ()
+    {
+        float yatay = Input.GetAxisRaw("Horizontal");
+        float dikey = Input.GetAxisRaw("Vertical");
+        
+        Vector3 vec = new Vector3(yatay,0,dikey);
+        fizik.AddForce(vec*hiz);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag=="Engel")
+        {
+            //Destroy(other.gameObject);
+            other.gameObject.SetActive(false);//is riggerini degiştirir
+            sayac++;
+            sayacText.text = "Sayac=" + sayac;
+        }
+        if (sayac==toplanicakNesneSayisi)
+        {
+            OyunBittiText.text = "OYUN BİTTİ!";
+        }
+    }
+}
---
+++ Roll a Ball/Assets/Kodlar/ToplanacakNesneler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToplanacakNesneler : MonoBehaviour {
+
+	
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        transform.Rotate(new Vector3(12,30,45)*Time.deltaTime);
+	}
+}
